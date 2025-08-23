232a7b7a03b693f46516d43ca188b92994b0b2c8
Sebadalpra <Sebastiandalpra@gmail.com>
2025-08-22 22:27:21 -0300

    primas modif

---
+++ tareas/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'bienvenida', pathMatch: 'full'
+    },
+    { path: 'login',
+        loadComponent: () => import('./pages/login/login').then(m => m.Login)
+    },
+    {
+        path: 'registro',
+        loadComponent: () => import('./pages/registro/registro').then(m => m.Registro)
+    },
+    {
+        path: 'bienvenida',
+        loadComponent: () => import('./pages/bienvenida/bienvenida').then(m => m.Bienvenida)
+    },
+    {
+        path: 'sobre-mi',
+        loadComponent: () => import('./pages/sobre-mi/sobre-mi').then(m => m.SobreMi)
+    },
+    {
+        path: '**',
+        redirectTo: 'error'
+    }
+];
